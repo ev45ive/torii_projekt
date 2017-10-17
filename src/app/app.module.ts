c8c60b8c25470831574959f395f85992d4ff7d3d
Mariusz Galinski <bc28@pjwstk.edu.pl>
2017-10-17 19:49:30 +0200

    #10 stworzenie home Componentu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Routing } from "./app.routing";
+import { MariuszHomeComponentComponent } from './mariusz-home-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MariuszHomeComponentComponent
   ],
   imports: [
     BrowserModule,
