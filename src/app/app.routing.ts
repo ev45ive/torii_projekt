cf1ecee18ee1a45fed906a5b9a69b14ab65201dc
roselew <rozalewinska@gmail.com>
2017-10-17 20:17:15 +0200

    #10 - roselew dodany (#15)

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,9 @@
 import { RouterModule, Routes } from '@angular/router'
+import { RoselewHomeComponent } from './roselew-home.component';
 import { MateuszHomeComponent } from './mateusz-home.component';
 
 const routes:Routes = [
+    { path:'roselew', component: RoselewHomeComponent},
     { path:'mateusz', component: MateuszHomeComponent}
 ]
 
