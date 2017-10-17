eb92152a8c07871201415b08553776e47794c3ab
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-10-17 20:22:49 +0200

    Routing fixes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,16 @@
 import { RouterModule, Routes } from '@angular/router'
 import { RoselewHomeComponent } from './roselew-home.component';
 import { MateuszHomeComponent } from './mateusz-home.component';
+import { DevHomeComponentComponent } from './dev-home-component.component';
+import { MariuszHomeComponentComponent } from './mariusz-home-component.component';
+import { NkamaszewskiHomeComponent } from './nkamaszewski-home.component';
 
 const routes:Routes = [
     { path:'roselew', component: RoselewHomeComponent},
-    { path:'mateusz', component: MateuszHomeComponent}
+    { path:'mateusz', component: MateuszHomeComponent},
+    { path:'dev', component: DevHomeComponentComponent},
+    { path:'mariusz', component: MariuszHomeComponentComponent},
+    { path:'nkamaszewski', component: NkamaszewskiHomeComponent}
 ]
 
 export const Routing = RouterModule.forRoot(routes)
