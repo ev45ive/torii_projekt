7351e4c70514051e5188f9a07ee9e2974b1073ab
OnlyManix <onlymanix@gmail.com>
2017-10-17 20:14:28 +0200

    #10 stworzenie home Componentu (#13)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Routing } from "./app.routing";
+import { MariuszHomeComponentComponent } from './mariusz-home-component.component';
 import { DevHomeComponentComponent } from './dev-home-component.component';
 import { NkamaszewskiHomeComponent } from './nkamaszewski-home.component';
 import { MateuszHomeComponent } from './mateusz-home.component';
@@ -11,6 +12,7 @@ import { MateuszHomeComponent } from './mateusz-home.component';
 @NgModule({
   declarations: [
     AppComponent,
+    MariuszHomeComponentComponent,
     DevHomeComponentComponent,
     NkamaszewskiHomeComponent,
     MateuszHomeComponent
