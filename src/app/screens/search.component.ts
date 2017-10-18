550a0edf1a08c2bd588a86b98a548e51a9f93ae8
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-10-18 18:29:35 +0200

    Search options params - model

--- src/app/screens/search.component.ts
+++ src/app/screens/search.component.ts
@@ -13,6 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  parameters = [
+    { label: "Wynagrodzenie", children: [], expanded: false },
+    {
+      label: "Wyksztalcenie", children: [
+        { label: "Podstawowe", children: [], expanded: false },
+        { label: "Srednie", children: [], expanded: false },
+        { label: "Wyzsze", children: [], expanded: false },
+      ], expanded: false
+    },
+    {
+      label: "Branza", children: [
+        {
+          label: "Medycyna", children: [
+            { label: "Chirurg", children: [], expanded: false },
+            { label: "Okulista", children: [], expanded: false },
+          ], expanded: false
+        },
+        { label: "IT", children: [], expanded: false },
+      ], expanded: false
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
