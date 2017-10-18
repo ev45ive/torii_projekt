2e705c207f3a014aaa3800da98ad415ec2e9410a
Mateusz Kulesza <m.kulesza@pjwstk.edu.pl>
2017-10-18 18:38:50 +0200

    Recursive params tree

--- src/app/views/options-tree.component.ts
+++ src/app/views/options-tree.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface Item{
+  label:string
+  children: Item[]
+  expanded:boolean
+}
+
 @Component({
   selector: 'options-tree',
   template: `
-    <p>
-      options-tree works!
-    </p>
+    <ul>
+      <li *ngFor="let param of params">
+        <span>{{param.label}}</span>
+        <options-tree *ngIf="param.children" [params]="param.children"></options-tree>
+      </li>
+    </ul>
   `,
   styles: []
 })
 export class OptionsTreeComponent implements OnInit {
 
   @Input()
-  params = []
+  params:Item[] = []
 
   constructor() { }
 
